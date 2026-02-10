58876e5e8ab96125953fbfa0b7ef694958da5764
dhirendra <shindedhirendra780@gmail.com>
2026-02-10 22:49:28 +0530

    initial commit (frontend setup)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const App = () => {
+  // prettier-ignore
+  const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  return (
+    <>
+      <div className="text-3xl bg-red-300 font-bold underline">App</div>
+
+      <Button>aaa </Button>
+    </>
+  )
+}
+
+export default App
