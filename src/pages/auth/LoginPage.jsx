785c402eba05f12aeef4a62967d34b95b31d9796
dhirendra <shindedhirendra780@gmail.com>
2026-02-12 00:12:00 +0530

    added login component

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,10 +1,22 @@
 import { Button } from '@/components/ui/Button'
+import { LoginForm } from '@/components/login-form'
 import { toast } from 'sonner'
 const LoginPage = () => {
   return (
     <div>
-      <h1>Login</h1>
-      <Button onClick={() => toast.success('Login successful!')}>Login</Button>
+      {/* <h1>Login</h1>
+      <Button onClick={() => toast.success('Login successful!')}>Login</Button> */}
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a href="#" className="flex items-center gap-2 self-center font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              {/* <GalleryVerticalEnd className="size-4" /> */}
+            </div>
+            Acme Inc.
+          </a>
+          <LoginForm />
+        </div>
+      </div>
     </div>
   )
 }
