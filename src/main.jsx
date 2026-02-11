32aa04e3c443946dd04f075b5b7c2df8e4e28f71
dhirendra <shindedhirendra780@gmail.com>
2026-02-11 23:42:45 +0530

    added sonner toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 import store from './store'
+import { Toaster } from '@/components/ui/sonner'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </StrictMode>
 )
--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/Button'
+import { toast } from 'sonner'
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>Login</h1>
+      <Button onClick={() => toast.success('Login successful!')}>Login</Button>
+    </div>
+  )
+}
 
-export default LoginPage;
+export default LoginPage
